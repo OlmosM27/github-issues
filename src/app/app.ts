083f139bd82ff20e6d0ac4c5d12f08d29f522127
martinOlmos <martin.olmos@ase-sinaloa.gob.mx>
2026-06-04 15:59:35 -0700

    [ADD]: Commit inicial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, isDevMode, signal, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { injectDevtoolsPanel } from '@tanstack/angular-query-devtools-experimental';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('github-issues');
+
+  private readonly devtoolsHost = viewChild<ElementRef>('devtoolsHost');
+
+  constructor() {
+    if (isDevMode()) {
+      injectDevtoolsPanel(() => ({ hostElement: this.devtoolsHost() }));
+    }
+  }
 }
---
+++ src/app/modules/issues/pages/issue/issue-page.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-page',
+  imports: [],
+  templateUrl: './issue-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class IssuePage {}
